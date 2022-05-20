bb67b19c68d306452b6929479bbf094848e62bf2
Nurgazy Ismanov <nurgazy.ismanov.kk@gmail.com>
2022-05-20 22:49:10 +0600

    add index.html

---
+++ JS/script3.js
@@ -0,0 +1,7 @@
+//3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+// let name = "Nurgazy";
+// document.write(name);
+
+// let number = 715600;
+// document.write(number);
